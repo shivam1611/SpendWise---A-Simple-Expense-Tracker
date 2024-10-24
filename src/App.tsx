d4364a7b7959e1868bfee94ba28c39bc1a393bda
codeshey <iamshivamsharma755@gmail.com>
2024-10-24 19:36:50 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-white bg-green-500 p-4 text-xl text-center">Hello Codeshey this is the starting of spendwise app</p>
+    </div>
+  )
+}
+
+export default App
